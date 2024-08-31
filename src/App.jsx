f62a94eb065c06a1bf7e89a30e505f15580c6f07
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-31 11:15:01 +0700

    Call API HelloWorld

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import axios from "./util/axios.customize";
+
+function App() {
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api/`);
+      console.log(res);
+    };
+
+    fetchHelloWorld();
+  }, []);
+
+  return <>Hello World</>;
+}
+
+export default App;
